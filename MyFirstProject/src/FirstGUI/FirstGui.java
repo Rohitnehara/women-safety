fdac250bcd2e5a5a7f4bd277a91f9072d2b28be9
Isha Parab <81077133+Isha-18@users.noreply.github.com>
2021-12-27 20:34:22 +0530

    IshaDidSomething

---
+++ MyFirstProject/src/FirstGUI/FirstGui.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import java.awt.*;
+
+
+
+
+
+public class FirstGui {
+    public static void main(String[] args) {
+    JFrame frame = new JFrame();
+    frame.setTitle("My First GUI");
+    frame.setSize(420,420);
+    frame.setVisible(true);
+
+
+
+    }
+
+}
---
+++ MyFirstProject/src/FirstGUI/GuiPractise.java
@@ -0,0 +1,12 @@
+package FirstGUI;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+ public class GuiPractise extends guii.FirstGui {
+    JFrame frame;
+    Border border= BorderFactory.createLineBorder(Color.red,3);
+
+
+ }
